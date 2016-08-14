9b42afbfb9f018a1bef16d12b18dffcf664c6a3a
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-15 01:00:25 +0200

    clean up

--- src/flappy-bird/object/Pipes.js
+++ src/flappy-bird/object/Pipes.js
@@ -17,31 +17,9 @@ class Pipes {
     this.pipesToCheckForAdd = new Array();
   }
 
-  destroyElement() {
-    this.pipes.forEach(pipe=> {
-      if(pipe.inCamera) {
-        pipe.alive = false;
-      }
-    });
-
-    this.pipesEndBottom.forEach(pipe=> {
-      if(pipe.inCamera) {
-        pipe.alive = false;
-      }
-    });
-
-    this.pipesEndTop.forEach(pipe=> {
-      if(pipe.inCamera) {
-        pipe.alive = false;
-      }
-    });
-  }
-
   update(game, ground) {
-    // Quand le premier tuyau se trouve au milieu du terrain
     if(this.pipesToCheckForAdd.length != 0 && this.pipesToCheckForAdd[0].x + this.pipesToCheckForAdd[0].width / 2 < game.world.width / 2) {
       this.pipesToCheckForAdd.splice(0, 1);
-      // On ajoute un nouveau tuyau
       this.addGroupPipes(game, ground);
     }
   }
@@ -67,45 +45,32 @@ class Pipes {
   }
 
   addPieceOfPipe(x, y, i, hole, nbPipe) {
-    // Si le trou est juste avant ou juste après, on place les pipeEnd
     if(i == hole + 2 || i == hole - 2) {
       const yDiff = 15;
       let pipeEnd;
       let yPipe;
 
       if(i == hole + 2) {
-        // On prend le premier élément "mort" du groupe pipesEndTop
         pipeEnd = this.getFirstAvailable(this.pipesEndTop);
         yPipe = y + yDiff;
       } else {
-        // On prend le premier élément "mort" du groupe pipesEndBottom
         pipeEnd = this.getFirstAvailable(this.pipesEndBottom);
         yPipe = y - yDiff;
       }
-      // On change la position du bout de tuyau
       pipeEnd.reset(x - 4, yPipe);
-      // On change la vitesse pour qu'il se déplace en même temps que le sol
       pipeEnd.body.velocity.x = -250;
-      // On supprime ce bout de tuyau s'il sort du terrain
       pipeEnd.outOfBoundsKill = true;
       pipeEnd.body.immovable = true;
     }
 
-    // On prend le premier élément "mort" du groupe pipes
     let pipe = this.getFirstAvailable(this.pipes);
-    // On change la position du bout de tuyau
     pipe.reset(x, y);
-    // On change la vitesse pour qu'il se déplace en même temps que le sol
     pipe.body.velocity.x = -250;
-    // On supprime ce bout de tuyau s'il sort du terrain
     pipe.outOfBoundsKill = true;
     pipe.body.immovable = true;
 
-    // si on se trouve sur le premier morceau de tuyau du groupe
     if(i == 0) {
-      // On enregistre le tuyau pour connaître la position de ce dernier et savoir quand augmenter le score
       this.pipesToCheckForScore.push(pipe);
-      // Idem pour savoir quand ajouter un nouveau groupe de tuyau
       this.pipesToCheckForAdd.push(pipe);
     }
   }
