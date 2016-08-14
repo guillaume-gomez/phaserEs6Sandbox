0c299136254a2e485353e945064675e0480fac48
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-14 18:23:45 +0200

    add floor

--- src/flappy-bird/states/GameState.js
+++ src/flappy-bird/states/GameState.js
@@ -7,6 +7,12 @@ class GameState extends Phaser.State {
     this.background.width = this.game.width;
     this.background.height = this.game.height;
 
+    this.ground = this.game.add.sprite(0, 0, 'ground');
+    this.ground.width = this.game.width * 2;
+    this.ground.y = this.game.height - this.ground.height;
+    this.game.physics.arcade.enable(this.ground);
+    this.ground.body.velocity.x = -250;
+
     this.bird =  new Bird(this.game, 200, 0, 'bird');
     this.game.add.existing(this.bird);
     this.game.input.onTap.add(this.start, this);
@@ -23,10 +29,15 @@ class GameState extends Phaser.State {
     this.game.load.atlasJSONHash('bird', 'res/bird.png', 'res/bird.json');
     // background
     this.game.load.image('background', 'res/background.png');
+    this.game.load.image('ground', 'res/ground.png');
   }
 
   update() {
+    if(this.ground.x + this.ground.width / 2 <= 0) {
+      this.ground.x = 0;
+    }
     this.bird.update();
+
   }
 
   start() {
