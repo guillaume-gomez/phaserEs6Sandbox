3e8f2bfd4fabe63e8297a21846c3bdd44e74fb74
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-14 15:16:08 +0200

    remove unused variablesé

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import GameState from 'states/GameState';
 class Game extends Phaser.Game {
 
   constructor() {
-    let map;
-    let tileset;
-    let layer;
-    let player;
-    let cursors;
     super(450, 800, Phaser.AUTO, 'content', null);
     this.score = 0;
     this.state.add('GameState', GameState, false);
