fecbf077b190ea81970bc7c9a7ae2c483338ed01
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-14 02:29:09 +0200

    clean GameState Class

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -3,13 +3,29 @@ import RainbowText from 'objects/RainbowText';
 class GameState extends Phaser.State {
 
   create() {
+    //Get the dimensions of the tile we are using
+    this.tileWidth = this.game.cache.getImage('tile').width;
+    this.tileHeight = this.game.cache.getImage('tile').height;
+
+    //Set the background colour to blue
+    this.game.stage.backgroundColor = '479cde';
+
+    //Enable the Arcade physics system
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //Add a platforms group to hold all of our tiles, and create a bunch of them
+    this.platforms = this.game.add.group();
+    this.platforms.enableBody = true;
+    this.platforms.createMultiple(250, 'tile');
   }
 
   preload() {
+    this.game.load.image('tile', 'res/tile.png');
+    this.game.load.image('player', 'res/player.png');
   }
 
   update() {
-  
+
   }
 
 }
