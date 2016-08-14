bb3938bcf9936196ad56733ace1f8ef4c65de8b2
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-14 03:27:37 +0200

    encapslutate player and some properties

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,6 +1,21 @@
-class Player {
+class Player extends Phaser.Sprite {
 
-  constructor() {
+  constructor(game, x, y, key, frame) {
+    super(game, x, y, key, frame);
+    //Set the players anchor point to be in the middle horizontally
+    this.anchor.setTo(0.5, 1.0);
+
+    //Enable physics on the player
+    game.physics.arcade.enable(this);
+
+    //Make the player fall by applying gravity
+    this.body.gravity.y = 2000;
+
+    //Make the player collide with the game boundaries
+    this.body.collideWorldBounds = true;
+
+    //Make the player bounce a little
+    this.body.bounce.y = 0.1;
   }
 
 }
