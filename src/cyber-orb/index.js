90ec2e54a33344fd5e014a7e8b336e9d915444c1
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-15 13:55:58 +0200

    set up Game instance

--- src/cyber-orb/index.js
+++ src/cyber-orb/index.js
@@ -3,7 +3,7 @@ import GameState from 'states/GameState';
 class Game extends Phaser.Game {
 
   constructor() {
-    super(450, 800, Phaser.AUTO, 'content', null);
+    super(320, 480, Phaser.AUTO, 'content', null);
     this.state.add('GameState', GameState, false);
     this.state.start('GameState');
   }
