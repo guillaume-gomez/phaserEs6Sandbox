606734457602639380fe518a3c3f66aa886168cc
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-15 15:30:58 +0200

    add  skeleton in GameState

--- src/cyber-orb/states/GameState.js
+++ src/cyber-orb/states/GameState.js
@@ -1,12 +1,47 @@
 class GameState extends Phaser.State {
 
   create() {
+
   }
 
   preload() {
+
   }
 
   update() {
+
+  }
+
+  initLevels() {
+
+  }
+
+  showLevel(level) {
+
+  }
+
+  updateCounter() {
+
+  }
+
+  managePause() {
+
+  }
+
+  manageAudio() {
+
+  }
+
+  wallCollision() {
+
+  }
+
+  handleOrientation(e) {
+
+  }
+
+  finishLevel() {
+
   }
 }
 
