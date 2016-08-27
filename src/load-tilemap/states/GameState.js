90cb5678c244d26697d1e9d9c4d2f9cdfe6d7c83
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-28 00:58:26 +0200

    init and display tileset

--- src/load-tilemap/states/GameState.js
+++ src/load-tilemap/states/GameState.js
@@ -1,12 +1,19 @@
+const PATH = "res/load-tilemap";
+
 class GameState extends Phaser.State {
 
   create() {
+    let map = this.game.add.tilemap('Map1');
+    map.addTilesetImage('Desert', 'Tileset');
+    map.createLayer('Ground');
   }
 
   update() {
   }
 
   preload() {
+     this.game.load.tilemap('Map1', PATH + "/my-tilemap.json", null, Phaser.Tilemap.TILED_JSON);
+     this.game.load.image('Tileset', PATH + "/tmw_desert_spacing.png");
   }
 }
 
