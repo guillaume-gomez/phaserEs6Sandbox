bb54a37ae46372b50f1b59bb1b4df600f55dac5e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-08-28 01:27:05 +0200

    init offset angle and move

--- src/load-tilemap/object/Car.js
+++ src/load-tilemap/object/Car.js
@@ -1,14 +1,35 @@
+const OFFSET = 90;
 class Car extends Phaser.Sprite {
 
   constructor(game, x, y, key, frame) {
     super(game, x, y, key, frame);
     this.game = game;
 
+    this.cursors = game.input.keyboard.createCursorKeys();
+    this.angle = OFFSET;
+
     //Enable physics on the player
     //game.physics.arcade.enable(this);
   }
 
   update() {
+    this.body.velocity.x = 0;
+    this.body.velocity.y = 0;
+    this.body.angularVelocity = 0;
+
+    if (this.cursors.left.isDown)
+    {
+        this.body.angularVelocity = -200;
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.body.angularVelocity = 200;
+    }
+
+    if (this.cursors.up.isDown)
+    {
+        this.body.velocity.copyFrom(this.game.physics.arcade.velocityFromAngle(this.angle - OFFSET, 300));
+    }
   }
 }
 
