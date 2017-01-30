8d44ce2deb4ca8f2d85eed4b6b12fa2fbdac2d9f
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-30 18:21:34 +0100

    setyp new example

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var STATIC_PATH = './static';
 var ENTRY_FILE = SOURCE_PATH + '/index.js';
 var OUTPUT_FILE = 'game.js';
 
-var examplesName = ["first-game", "flappy-bird", "cyber-orb", "platformer", "load-tilemap", "p2-physics"];
+var examplesName = ["first-game", "flappy-bird", "cyber-orb", "platformer", "load-tilemap", "p2-physics", "map-generator"];
 
 var keepFiles = false;
 
