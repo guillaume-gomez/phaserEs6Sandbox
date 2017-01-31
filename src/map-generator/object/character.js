a23c93b40de89c9b6c74218ea92949970ae2b310
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-31 18:30:09 +0100

    fix character code

--- src/map-generator/object/character.js
+++ src/map-generator/object/character.js
@@ -1,28 +1,27 @@
 class Character extends Phaser.Sprite {
 
-  constructor(game, x, y) {
-    super(game, x, y, "Character", 0);
-    this.game = game;
-
+  constructor(game, x, y, key, frame) {
+    super(game, x, y, key, frame);
+    game.physics.enable(this);
     this.cursors = game.input.keyboard.createCursorKeys();
   }
 
   update() {
     if (this.cursors.left.isDown)
     {
-        this.player.body.velocity.x = -200;
+        this.body.velocity.x = -200;
     }
     else if (this.cursors.right.isDown)
     {
-        this.player.body.velocity.x = 200;
+        this.body.velocity.x = 200;
     }
 
     if (this.cursors.up.isDown)
     {
-        this.player.body.velocity.y = -200;
+        this.body.velocity.y = -200;
     }
     else if (this.cursors.down.isDown) {
-        this.player.body.velocity.y = 200;
+        this.body.velocity.y = 200;
     }
   }
 }
