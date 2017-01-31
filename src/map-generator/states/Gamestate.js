44dc7e0b12a4043ee60bbd7168c11f6bbc0a02b6
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-01-31 18:30:27 +0100

    instanciate character

--- src/map-generator/states/Gamestate.js
+++ src/map-generator/states/Gamestate.js
@@ -1,16 +1,20 @@
 const PATH = "res/map-generator";
+import Character from 'object/Character';
 
 class GameState extends Phaser.State {
 
   create() {
     this.game.stage.backgroundColor = "#4488AA";
+    this.character = new Character(this.game, 200, 200, 'Character', 0);
+    this.game.add.existing(this.character);
+    this.game.camera.follow(this.character);
   }
 
   update() {
   }
 
   preload() {
-    this.game.load.image('Character', PATH + 'character.png');
+    this.game.load.image('Character', PATH + '/character.png');
   }
 }
 
