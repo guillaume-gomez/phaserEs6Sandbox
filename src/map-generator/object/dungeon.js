729c3e96803c029076bd5c22cd56a10902078f51
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-02 18:56:42 +0100

    create new class to manage rooms

---
+++ src/map-generator/object/dungeon.js
@@ -0,0 +1,9 @@
+class Dungeon extends Phaser.Group {
+
+  constructor(game, parent, name, addToStage, enableBody, physicsBodyTypet) {
+    super(game, parent, name, addToStage, enableBody, physicsBodyType);
+  }
+
+}
+
+export default Dungeon;
