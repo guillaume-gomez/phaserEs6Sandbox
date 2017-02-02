e5b65cbcc103fa2866bc4fc916a5fec3716749cc
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-02 18:46:09 +0100

    create a red room

--- src/map-generator/object/room.js
+++ src/map-generator/object/room.js
@@ -1,8 +1,18 @@
+const Color ='#ff00ff';
+
 class Room extends Phaser.Sprite {
 
-  constructor(game, x, y, key, frame) {
-    super(game, x, y, key, frame);
+  constructor(game, width, height) {
+    let bmd = game.add.bitmapData(width,height);
+    // draw to the canvas context like normal
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0,0,width,height);
+    bmd.ctx.fillStyle = '#ff0000';
+    bmd.ctx.fill();
+
+    super(game,width,height, bmd);
   }
+
 }
 
 export default Room;
