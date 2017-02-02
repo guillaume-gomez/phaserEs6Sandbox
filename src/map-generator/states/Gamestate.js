8e89e3db6a1be380aa201b6f0859eb62491a3e25
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-02 18:46:39 +0100

    add room to test

--- src/map-generator/states/Gamestate.js
+++ src/map-generator/states/Gamestate.js
@@ -1,12 +1,15 @@
 const PATH = "res/map-generator";
 import Character from 'object/Character';
+import Room from 'object/Room';
 
 class GameState extends Phaser.State {
 
   create() {
     this.game.stage.backgroundColor = "#4488AA";
     this.character = new Character(this.game, 200, 200, 'Character', 0);
+    this.room = new Room(this.game,200,200);
     this.game.add.existing(this.character);
+    this.game.add.existing(this.room);
     this.game.camera.follow(this.character);
   }
 
