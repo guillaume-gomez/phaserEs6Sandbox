b1a3123a71b7939ddd02d1640284de521989e926
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-03 16:10:25 +0100

    algo to create dungeon

--- src/map-generator/object/dungeon.js
+++ src/map-generator/object/dungeon.js
@@ -1,15 +1,15 @@
 import Room from './Room';
 
 const MaxRoom = 10;
-const MinRoomSize = 10;
-const MaxRoomSize = 100;
+const MinRoomSize = 50;
+const MaxRoomSize = 200;
 
 const MapWidth = 500;
 const MapHeight = 800;
 
 class Dungeon extends Phaser.Group {
 
-  constructor(game, parent, name, addToStage, enableBody, physicsBodyTypet) {
+  constructor(game, parent, name, addToStage, enableBody, physicsBodyType) {
     super(game, parent, name, addToStage, enableBody, physicsBodyType);
 
     for(let i = 0; i < MaxRoom; i++) {
@@ -18,13 +18,24 @@ class Dungeon extends Phaser.Group {
       const x = Math.random() * (MapWidth - width - 1) + 1;
       const y = Math.random() * (MapHeight - height - 1) + 1;
 
-      let newRoom = new Room(x, y, width, height);
+      let newRoom = new Room(game, x, y, width, height);
       let failed = false;
+      this.children.some(child => {
+         failed = newRoom.overlapRoom(child);
+         if(failed) {
+          //exit le loop
+          return true;
+         }
+      });
+      if (!failed) {
+        // local function to carve out new room
+        this.createRoom(newRoom);
+      }
     }
   }
 
   createRoom(room) {
-
+    this.add(room);
   }
 
 }
