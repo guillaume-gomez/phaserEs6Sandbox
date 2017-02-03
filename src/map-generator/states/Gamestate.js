9d05856f306d5fad85c8ea00029f5f4065223fc8
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-03 16:10:34 +0100

    call dungeon

--- src/map-generator/states/Gamestate.js
+++ src/map-generator/states/Gamestate.js
@@ -1,14 +1,13 @@
 const PATH = "res/map-generator";
 import Character from 'object/Character';
-import Room from 'object/Room';
+import Dungeon from 'object/dungeon';
 
 class GameState extends Phaser.State {
 
   create() {
     this.game.stage.backgroundColor = "#4488AA";
     this.character = new Character(this.game, 50, 200, 'Character', 0);
-    this.room = new Room(this.game,200,200);
-    this.game.add.existing(this.room);
+    this.dungeon = new Dungeon(this.game);
     this.game.add.existing(this.character);
     this.game.camera.follow(this.character);
   }
