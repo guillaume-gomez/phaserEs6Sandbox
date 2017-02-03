321f8c5f1c46e06af14bd8e09c27b345edbc5307
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-03 16:10:08 +0100

    add personal detection

--- src/map-generator/object/room.js
+++ src/map-generator/object/room.js
@@ -1,4 +1,4 @@
-const Color = "473B3B";
+const Color = "#473B3B";
 
 class Room extends Phaser.Sprite {
 
@@ -12,6 +12,14 @@ class Room extends Phaser.Sprite {
 
     super(game,x,y, bmd);
   }
+
+    overlapRoom(room) {
+        if (this.x + this.width < room.x) return false; // a is left of b
+        if (this.x > room.x + room.width) return false; // a is right of b
+        if (this.y + this.height < room.y) return false; // a is above b
+        if (this.y > room.y + room.height) return false; // a is below b
+        return true; // boxes overlap
+    }
 
 }
 
