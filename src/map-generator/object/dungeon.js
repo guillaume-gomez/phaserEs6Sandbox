c78b9cde687c16ad1d35688403f24d2df76c3c4e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-03 15:03:47 +0100

    add new class dungeon

--- src/map-generator/object/dungeon.js
+++ src/map-generator/object/dungeon.js
@@ -1,7 +1,30 @@
+import Room from './Room';
+
+const MaxRoom = 10;
+const MinRoomSize = 10;
+const MaxRoomSize = 100;
+
+const MapWidth = 500;
+const MapHeight = 800;
+
 class Dungeon extends Phaser.Group {
 
   constructor(game, parent, name, addToStage, enableBody, physicsBodyTypet) {
     super(game, parent, name, addToStage, enableBody, physicsBodyType);
+
+    for(let i = 0; i < MaxRoom; i++) {
+      const width = MinRoomSize + Math.random() * (MaxRoomSize - MinRoomSize + 1);
+      const height = MinRoomSize + Math.random() * (MaxRoomSize - MinRoomSize + 1);
+      const x = Math.random() * (MapWidth - width - 1) + 1;
+      const y = Math.random() * (MapHeight - height - 1) + 1;
+
+      let newRoom = new Room(x, y, width, height);
+      let failed = false;
+    }
+  }
+
+  createRoom(room) {
+
   }
 
 }
