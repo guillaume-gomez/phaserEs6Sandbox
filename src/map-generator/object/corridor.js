1d258281ea68b73a5d8e7456068369aca75df240
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-04 19:11:24 +0100

    add corridor

---
+++ src/map-generator/object/corridor.js
@@ -0,0 +1,26 @@
+const Color = "#473B3B";
+
+class Corridor extends Phaser.Sprite {
+
+  constructor(game, x,y, width, height) {
+    let bmd = game.add.bitmapData(width,height);
+    // draw to the canvas context like normal
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0,0,width,height);
+    bmd.ctx.fillStyle = Color;
+    bmd.ctx.fill();
+
+    super(game,x,y, bmd);
+  }
+
+    overlapRoom(room) {
+        if (this.x + this.width < room.x) return false; // a is left of b
+        if (this.x > room.x + room.width) return false; // a is right of b
+        if (this.y + this.height < room.y) return false; // a is above b
+        if (this.y > room.y + room.height) return false; // a is below b
+        return true; // boxes overlap
+    }
+
+}
+
+export default Corridor;
