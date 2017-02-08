522c486f3c517a1e12c1de343d3933367b19c0a7
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-08 19:16:45 +0100

    fix vertical collidor + add tools function

--- src/map-generator/object/dungeon.js
+++ src/map-generator/object/dungeon.js
@@ -2,12 +2,12 @@ import Room from './Room';
 import Corridor from './Corridor';
 
 const CorridorHeight = 50;
-const MaxRoom = 10;
+const MaxRoom = 2;
 const MinRoomSize = 50;
-const MaxRoomSize = 200;
+const MaxRoomSize = 50;
 
 const MapWidth = 500;
-const MapHeight = 800;
+const MapHeight = 500;
 
 class Dungeon extends Phaser.Group {
 
@@ -19,11 +19,16 @@ class Dungeon extends Phaser.Group {
       const height = MinRoomSize + Math.random() * (MaxRoomSize - MinRoomSize + 1);
       const x = Math.random() * (MapWidth - width - 1) + 1;
       const y = Math.random() * (MapHeight - height - 1) + 1;
+      console.log(x)
+      console.log(y)
+      console.log(width)
+      console.log(height)
+      console.log("+++++++++++++++++++++++++")
 
       let newRoom = new Room(game, x, y, width, height);
       let failed = false;
       this.children.some(child => {
-         failed = newRoom.overlapRoom(child);
+         failed = false//newRoom.overlapRoom(child);
          if(failed) {
           //exit le loop
           return true;
@@ -33,20 +38,33 @@ class Dungeon extends Phaser.Group {
         // local function to carve out new room
         this.createRoom(newRoom);
         const newCenter = newRoom.center;
-
-        if(this.children.length != 0) {
-          const prevCenter = this.children[this.children.length -1].center;
+        if(this.rooms().length > 1) {
+          const prevCenter = this.findLastRoom(newRoom).center;
+          console.log(prevCenter)
+          console.log(newCenter)
+          console.log("----------")
           const rng = Math.random() * 2;
           if(rng >= 1) {
-            this.horizontalCorridor(game,prevCenter.x, newCenter.x, prevCenter.y);
+            //this.horizontalCorridor(game,prevCenter.x, newCenter.x, prevCenter.y);
             this.verticalCorridor(game, prevCenter.y, newCenter.y, newCenter.x);
           } else {
             this.verticalCorridor(game, prevCenter.y, newCenter.y, prevCenter.x);
-            this.horizontalCorridor(game,prevCenter.x, newCenter.x, newCenter.y);
+            //this.horizontalCorridor(game,prevCenter.x, newCenter.x, newCenter.y);
           }
         }
       }
     }
+  }
+
+  rooms(newRoom = null) {
+    return this.children.filter(child => {
+      return child.name === "room" && child != newRoom;
+    });
+  }
+
+  findLastRoom(newRoom) {
+    const rooms = this.rooms(newRoom);
+    return rooms[rooms.length - 1];
   }
 
   createRoom(room) {
@@ -54,12 +72,15 @@ class Dungeon extends Phaser.Group {
   }
 
   horizontalCorridor(game, x1, x2, y) {
+    const width = Math.max((x2-x1), 50);
     const corridor = new Corridor(game, x1, y, (x2 - x1), CorridorHeight);
     this.add(corridor);
   }
 
   verticalCorridor(game, y1, y2, x) {
-    const corridor = new Corridor(game, x, y1, CorridorHeight, (y2 - y1));
+    const height = Math.max(Math.abs(y2 - y1), 50);
+    const originY = Math.min(y1,y2)
+    const corridor = new Corridor(game, x, originY, CorridorHeight, height);
     this.add(corridor);
   }
 
