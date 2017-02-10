9233b7bdd290bfce77a01dd20b575b622b15a89c
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-11 00:11:49 +0100

    insert colission in the demo

--- src/map-generator/states/Gamestate.js
+++ src/map-generator/states/Gamestate.js
@@ -2,6 +2,8 @@ const PATH = "res/map-generator";
 import Character from 'object/Character';
 import Dungeon from 'object/dungeon';
 
+import {isInside} from "object/utils";
+
 class GameState extends Phaser.State {
 
   create() {
@@ -9,11 +11,18 @@ class GameState extends Phaser.State {
     this.game.world.setBounds(0, 0, 2000, 2000);
     this.character = new Character(this.game, 50, 200, 'Character', 0);
     this.dungeon = new Dungeon(this.game);
+    const roomPosition = this.dungeon.getInitialRoom().center;
+    this.character.position.setTo(roomPosition.x,roomPosition.y);
     this.game.add.existing(this.character);
     this.game.camera.follow(this.character);
   }
 
   update() {
+    if(!this.dungeon.collide(this.character)) {
+      this.character.x = this.character.oldPosition.x
+      this.character.y = this.character.oldPosition.y
+    }
+    this.character.oldPosition = {x: this.character.position.x, y: this.character.position.y};
   }
 
   preload() {
