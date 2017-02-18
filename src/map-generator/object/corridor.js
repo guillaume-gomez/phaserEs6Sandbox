f3dc50147e10d22e5439c8b1879582388cd6be02
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-18 19:10:15 +0100

    add tools method to build walls

--- src/map-generator/object/corridor.js
+++ src/map-generator/object/corridor.js
@@ -23,10 +23,10 @@ class Corridor extends Phaser.Group {
   addVerticalWall(game, x, y, width, height) {
     const corridorSprite = new CorridorSprite(game, x, y, width, height);
      for(let i = y; i < y + height; i += WallSize) {
-      const upWall = game.add.sprite(x - WallSize, i, 'Wall');
-      const bottomWall = game.add.sprite(x + width, i, 'Wall');
-      this.add(upWall);
-      this.add(bottomWall);
+      let leftWall = this.addWall(game, x - WallSize, i);
+      let rightWall = this.addWall(game, x + width, i);
+      this.add(leftWall);
+      this.add(rightWall);
     }
     this.add(corridorSprite);
   }
@@ -34,15 +34,23 @@ class Corridor extends Phaser.Group {
   addHorizontalWall(game, x, y, width, height) {
     const corridorSprite = new CorridorSprite(game, x, y, width, height);
     for(let i = x; i < x + width; i += WallSize) {
-      const upWall = game.add.sprite(i, y - WallSize, 'Wall');
-      const bottomWall = game.add.sprite(i, y + height, 'Wall');
+      let upWall = this.addWall(game, i, y - WallSize);
+      let bottomWall = this.addWall(game, i, y + height);
       this.add(upWall);
       this.add(bottomWall);
     }
     this.add(corridorSprite);
   }
 
+  addWall(game, x, y) {
+    let wall = game.add.sprite(x, y, 'Wall');
+    wall.name = "colissionWall";
+    game.physics.enable(wall, Phaser.Physics.ARCADE);
+    wall.body.immovable = true;
+    return wall;
+  }
 
+  //remove useless return in anonymous function
   getCorridorSprite() {
     const corridorSprite = this.children.find(child => {return child.name === "corridorSprite"; });
     if(!corridorSprite) {
@@ -51,6 +59,12 @@ class Corridor extends Phaser.Group {
     }
     return corridorSprite;
   }
+
+  walls() {
+    return this.children.filter(child => {return child.name == "colissionWall"; });
+  }
+
+
 }
 
 export default Corridor;
