4599484f809015b01efa65f126bb8216ae78b346
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-18 21:43:06 +0100

    add borders

--- src/map-generator/object/room.js
+++ src/map-generator/object/room.js
@@ -1,15 +1,36 @@
-import RoomSprite from './RoomSprite';
+const WallSize = 16;
+import RoomSprite from './roomSprite';
 
 class Room extends Phaser.Group {
 
-  constructor(game, parent, x,y, width, height) {
+  constructor(game, parent, x, y, width, height) {
     super(game, parent, "room", true, true, Phaser.Physics.ARCADE);
     this.createRoom(game, x, y, width, height);
   }
 
   createRoom(game, x, y, width, height) {
+    for(let i = x - WallSize; i < x + width + WallSize; i += WallSize) {
+      const upWall = this.addWall(game, i, y - WallSize);
+      const downWall = this.addWall(game, i, y + height);
+      this.add(upWall);
+      this.add(downWall);
+    }
+    for(let j = y; j < y + height; j += WallSize) {
+      const leftWall = this.addWall(game, x - WallSize, j);
+      const rightWall = this.addWall(game, x + width, j);
+      this.add(leftWall);
+      this.add(rightWall);
+    }
     const room = new RoomSprite(game, x, y, width, height);
     this.add(room);
+  }
+
+  addWall(game, x, y) {
+    let wall = game.add.sprite(x, y, 'Wall');
+    wall.name = "RoomWall";
+    game.physics.enable(wall, Phaser.Physics.ARCADE);
+    wall.body.immovable = true;
+    return wall;
   }
 
   borders() {
@@ -25,7 +46,6 @@ class Room extends Phaser.Group {
   }
 
   overlapRoom(room) {
-    console.log(this.roomSprite())
     this.roomSprite().overlapRoom(room);
   }
 
