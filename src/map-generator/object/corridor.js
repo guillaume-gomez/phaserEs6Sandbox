8da501bc3900d9be5e1c0a2f719199e4755c8936
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-02-28 10:25:19 +0100

    save direction as attribute

--- src/map-generator/object/corridor.js
+++ src/map-generator/object/corridor.js
@@ -10,11 +10,12 @@ class Corridor extends Phaser.Group {
     if(!Directions.includes(direction)) {
       console.error(`Corridor:constructor : ${direction} is a not a value possible [${Directions}]`);
     }
-    this.addWalls(game, x, y, width, height, direction);
+    this.direction = direction;
+    this.addWalls(game, x, y, width, height);
   }
 
-  addWalls(game, x,y, width, height, direction) {
-    if(direction === "vertical") {
+  addWalls(game, x,y, width, height) {
+    if(this.direction === "vertical") {
       this.addVerticalWall(game, x, y, width, height);
     } else {
       this.addHorizontalWall(game, x, y, width, height);
