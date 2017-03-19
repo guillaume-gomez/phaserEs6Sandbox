0dbf85d527980fc87b406d2c6cbca44e8e9dddfe
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-03-19 12:06:11 +0100

    forgot to export constants

--- src/map-generator/lib/constants.js
+++ src/map-generator/lib/constants.js
@@ -2,9 +2,9 @@ export const Color = "#473B3B";
 export const WallColor = "#395992";
 export const WallSize = 16;
 
-const Vertical = "vertical";
-const Horizontal = "horizontal"
-const Directions = [Vertical, Horizontal];
+export const Vertical = "vertical";
+export const Horizontal = "horizontal"
+export const Directions = [Vertical, Horizontal];
 
 export const WorldWitdth = 2000;
 export const WorldHeight = 2000;
