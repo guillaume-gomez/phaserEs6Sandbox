e20d3872091a4a3c0146f4266d8dd1dfebf9365e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-04-02 18:58:13 +0200

    fix algorithm

--- src/map-generator/constants/constants.js
+++ src/map-generator/constants/constants.js
@@ -6,6 +6,6 @@ export const CarpetHeight = 40;
 export const CornerWidth = 20;
 export const CornerHeight = 20;
 
-export const WorldWitdth = 1500;
-export const WorldHeight = 1500;
+export const WorldWitdth = 2000;
+export const WorldHeight = 2000;
 
