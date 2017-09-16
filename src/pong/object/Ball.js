21e559b8892ca2f33a921724f2519bfc99b84cf5
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-09-16 18:52:35 +0200

    fix collision ball

--- src/pong/object/Ball.js
+++ src/pong/object/Ball.js
@@ -1,8 +1,7 @@
 class Ball extends Phaser.Sprite {
 
-  constructor(game, x, y, color = '#0000ff') {
+  constructor(game, x, y, color = '#0000ff', velX, velY) {
     const bmd = game.add.bitmapData(50, 50);
-    // draw to the canvas context like normal
     bmd.ctx.beginPath();
     bmd.ctx.arc(25, 25, 25 - 5, 0, 2 * Math.PI, false);
     bmd.ctx.fillStyle = 'green';
@@ -12,6 +11,10 @@ class Ball extends Phaser.Sprite {
     bmd.ctx.stroke();
     super(game, x, y, bmd);
     game.physics.enable(this, Phaser.Physics.ARCADE);
+    this.checkWorldBounds = true;
+    this.body.collideWorldBounds = true;
+    this.anchor.set(0.5, 0.5);
+    this.body.velocity.setTo(velX, velY);
     this.body.bounce.set(1);
   }
 
