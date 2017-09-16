cbdab71cbe3b466afc0e67ad0c63036c2f6eee2a
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-09-16 19:01:08 +0200

    fix rendering

--- src/pong/states/GameState.js
+++ src/pong/states/GameState.js
@@ -8,14 +8,9 @@ class GameState extends Phaser.State {
 
   create() {
     this.game.stage.backgroundColor = '#182d3b';
-    this.backgroundGraphics = this.game.add.graphics(0, 0);
-    this.backgroundGraphics.lineStyle(2, 0xFF0000, 1);
-     for (var y = 0; y < HeightScreen; y += 5 * 2) {
-            this.backgroundGraphics.moveTo(this.game.world.centerX, y);
-            this.backgroundGraphics.lineTo(this.game.world.centerX, y + 5);
-        }
     // Start the Arcade physics system (for movements and collisions)
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.initMiddleLine();
 
     this.bar = new Bar(this.game, 200, 20);
     this.bar2 = new Bar(this.game, 200, HeightScreen - 20);
@@ -30,16 +25,25 @@ class GameState extends Phaser.State {
     this.cursors = this.game.input.keyboard.createCursorKeys();
   }
 
+  initMiddleLine() {
+    this.backgroundGraphics = this.game.add.graphics(0, 0);
+    this.backgroundGraphics.lineStyle(2, 0xFF00F0, 1);
+    for (var y = 0; y < HeightScreen; y += 5 * 2) {
+      this.backgroundGraphics.moveTo(this.game.world.centerX, y);
+      this.backgroundGraphics.lineTo(this.game.world.centerX, y + 5);
+    }
+  }
+
   update() {
     this.bar.body.velocity.x = 0;
     this.bar.body.velocity.y = 0;
     if (this.cursors.left.isDown)
     {
-        this.bar.body.velocity.x = -200;
+        this.bar.body.velocity.x = -500;
     }
     else if (this.cursors.right.isDown)
     {
-        this.bar.body.velocity.x= 200;
+        this.bar.body.velocity.x= 500;
     }
 
     this.game.physics.arcade.collide(this.ball, this.bar, null, this.updateBall, this);
