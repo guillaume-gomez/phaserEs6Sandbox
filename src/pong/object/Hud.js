cc2ad15469ca906f42a1e9b80dbd656df8eb5b7e
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-09 14:24:57 +0200

    add switching hud

--- src/pong/object/Hud.js
+++ src/pong/object/Hud.js
@@ -1,24 +1,23 @@
-import { WidthScreen, scoreToWin } from "../constants.js";
+import { WidthScreen, HeightScreen, scoreToWin } from "../constants.js";
 const style = {font: '80px Arial', fill: '#FFFFFF', align: 'center'}
 const styleWinner = {font: '50px Arial', fill: '#FFFFFF', align: 'center'}
-const top = 10;
+const top = 50;
+const left = WidthScreen - 80;
 const winnerLabel =  "Winner !";
 
 class Hud extends Phaser.Group {
 
-  constructor(game, scores) {
+  constructor(game, scores, direction = "vertical") {
     super(game);
-    this.scoreLeft = game.add.text(WidthScreen * 0.25, top, scores[0], style);
-    this.scoreLeft.anchor.set(0.5, 0);
-    this.winnerLeft = game.add.text(WidthScreen * 0.25, top + 100, winnerLabel, styleWinner);
-    this.winnerLeft.anchor.set(0.5, 0.5);
-    this.winnerLeft.visible = false;
+    this.switchHub(game, scores, direction);
 
-    this.scoreRight = game.add.text(WidthScreen * 0.75, top, scores[1], style);
-    this.scoreRight.anchor.set(0.5, 0);
-    this.winnerRight = game.add.text(WidthScreen * 0.75, top + 100, winnerLabel, styleWinner);
+    this.scoreLeft.anchor.set(0.5, 0.5);
+    this.winnerLeft.anchor.set(0.5, 0.5);
+    //this.winnerLeft.visible = false;
+
+    this.scoreRight.anchor.set(0.5, 0.5);
     this.winnerRight.anchor.set(0.5, 0.5);
-    this.winnerRight.visible = false;
+    //this.winnerRight.visible = false;
 
     this.add(this.scoreRight);
     this.add(this.scoreLeft);
@@ -40,6 +39,23 @@ class Hud extends Phaser.Group {
   makeWinnerInvisible(scores) {
     this.winnerLeft.visible = false;
     this.winnerRight.visible = false;
+  }
+
+  switchHub(game, scores, direction) {
+    if(direction === "vertical") {
+      this.scoreLeft = game.add.text(WidthScreen * 0.25, top, scores[0], style);
+      this.winnerLeft = game.add.text(WidthScreen * 0.25, top + 100, winnerLabel, styleWinner);
+
+      this.scoreRight = game.add.text(WidthScreen * 0.75, top, scores[1], style);
+      this.winnerRight = game.add.text(WidthScreen * 0.75, top + 100, winnerLabel, styleWinner);
+    } else {
+      this.scoreLeft = game.add.text(left, HeightScreen * 0.25, scores[0], style);
+      this.winnerLeft = game.add.text(left - 20, HeightScreen * 0.25 + 100, winnerLabel, styleWinner);
+
+      this.scoreRight = game.add.text(left, HeightScreen * 0.75, scores[1], style);
+      this.winnerRight = game.add.text(left - 20, HeightScreen * 0.75 + 100, winnerLabel, styleWinner);
+    }
+
   }
 
 }
