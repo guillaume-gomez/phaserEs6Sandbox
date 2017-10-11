1143b4ac07152ab01e74b78c75e9100ef6a42c26
Gomez Guillaume <guillaume.gomez846@gmail.com>
2017-10-11 09:53:52 +0200

    deploy

--- build/scripts/pong.js
+++ build/scripts/pong.js
@@ -166,28 +166,29 @@ function _inherits(subClass, superClass) {
 
 var style = { font: '80px Arial', fill: '#FFFFFF', align: 'center' };
 var styleWinner = { font: '50px Arial', fill: '#FFFFFF', align: 'center' };
-var top = 10;
+var top = 50;
+var left = _constants.WidthScreen - 80;
 var winnerLabel = "Winner !";
 
 var Hud = function (_Phaser$Group) {
   _inherits(Hud, _Phaser$Group);
 
   function Hud(game, scores) {
+    var orientation = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "vertical";
+
     _classCallCheck(this, Hud);
 
     var _this = _possibleConstructorReturn(this, (Hud.__proto__ || Object.getPrototypeOf(Hud)).call(this, game));
 
-    _this.scoreLeft = game.add.text(_constants.WidthScreen * 0.25, top, scores[0], style);
-    _this.scoreLeft.anchor.set(0.5, 0);
-    _this.winnerLeft = game.add.text(_constants.WidthScreen * 0.25, top + 100, winnerLabel, styleWinner);
-    _this.winnerLeft.anchor.set(0.5, 0.5);
-    _this.winnerLeft.visible = false;
+    _this.switchHub(game, scores, orientation);
 
-    _this.scoreRight = game.add.text(_constants.WidthScreen * 0.75, top, scores[1], style);
-    _this.scoreRight.anchor.set(0.5, 0);
-    _this.winnerRight = game.add.text(_constants.WidthScreen * 0.75, top + 100, winnerLabel, styleWinner);
+    _this.scoreLeft.anchor.set(0.5, 0.5);
+    _this.winnerLeft.anchor.set(0.5, 0.5);
+    //this.winnerLeft.visible = false;
+
+    _this.scoreRight.anchor.set(0.5, 0.5);
     _this.winnerRight.anchor.set(0.5, 0.5);
-    _this.winnerRight.visible = false;
+    //this.winnerRight.visible = false;
 
     _this.add(_this.scoreRight);
     _this.add(_this.scoreLeft);
@@ -213,6 +214,41 @@ var Hud = function (_Phaser$Group) {
     key: 'makeWinnerInvisible',
     value: function makeWinnerInvisible(scores) {
       this.winnerLeft.visible = false;
+      this.winnerRight.visible = false;
+    }
+  }, {
+    key: 'switchHub',
+    value: function switchHub(game, scores, orientation) {
+      if (this.scoreLeft || this.winnerLeft) {
+        this.scoreLeft.kill();
+        this.winnerLeft.kill();
+      }
+
+      if (this.scoreRight || this.winnerRight) {
+        this.scoreRight.kill();
+        this.winnerRight.kill();
+      }
+
+      if (orientation === "vertical") {
+        this.scoreLeft = game.add.text(_constants.WidthScreen * 0.25, top, scores[0], style);
+        this.winnerLeft = game.add.text(_constants.WidthScreen * 0.25, top + 100, winnerLabel, styleWinner);
+
+        this.scoreRight = game.add.text(_constants.WidthScreen * 0.75, top, scores[1], style);
+        this.winnerRight = game.add.text(_constants.WidthScreen * 0.75, top + 100, winnerLabel, styleWinner);
+      } else {
+        this.scoreLeft = game.add.text(left, _constants.HeightScreen * 0.25, scores[0], style);
+        this.winnerLeft = game.add.text(left - 20, _constants.HeightScreen * 0.25 + 50, winnerLabel, styleWinner);
+
+        this.scoreRight = game.add.text(left, _constants.HeightScreen * 0.75, scores[1], style);
+        this.winnerRight = game.add.text(left - 20, _constants.HeightScreen * 0.75 + 50, winnerLabel, styleWinner);
+      }
+
+      this.scoreLeft.anchor.set(0.5, 0.5);
+      this.winnerLeft.anchor.set(0.5, 0.5);
+      this.winnerLeft.visible = false;
+
+      this.scoreRight.anchor.set(0.5, 0.5);
+      this.winnerRight.anchor.set(0.5, 0.5);
       this.winnerRight.visible = false;
     }
   }]);
@@ -316,7 +352,7 @@ function _inherits(subClass, superClass) {
   }subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
 }
 
-var width = 25;
+var width = 200;
 
 var Switch = function (_Phaser$Sprite) {
   _inherits(Switch, _Phaser$Sprite);
@@ -414,7 +450,7 @@ var GameState = function (_Phaser$State) {
       this.game.stage.backgroundColor = '#182d3b';
       // Start the Arcade physics system (for movements and collisions)
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.orientation = "vertical";
+      this.orientation = "horizontal";
       this.player1Score = 0;
       this.player2Score = 0;
       this.initBoundaries();
@@ -425,7 +461,7 @@ var GameState = function (_Phaser$State) {
       this.ball.events.onOutOfBounds.add(this.ballOutOfBounds, this);
       this.game.add.existing(this.ball);
 
-      this.hud = new _Hud2.default(this.game, [this.player1Score, this.player2Score]);
+      this.hud = new _Hud2.default(this.game, [this.player1Score, this.player2Score], this.orientation);
       this.game.add.existing(this.hud);
 
       this.start();
@@ -586,6 +622,7 @@ var GameState = function (_Phaser$State) {
       this.initPaddlesPosition();
       this.initBoundaries();
       this.initSwitch();
+      this.hud.switchHub(this.game, [this.player1Score, this.player2Score], this.orientation);
     }
   }, {
     key: 'handleInput',
