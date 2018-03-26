a54369e5bd5aaed7f288834eb9e5733ab8e92ac5
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-26 23:48:19 +0200

    can move rectangle

--- src/rough/states/GameState.js
+++ src/rough/states/GameState.js
@@ -5,14 +5,38 @@ class GameState extends Phaser.State {
   create() {
     this.game.stage.backgroundColor = "#4488AA";
     const rsg = new RoughSpriteGenerator(this.game, 128, 128)
+
     this.sprite = rsg.getRectangleSprite(this.game, 0, 0);
+    this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
   }
 
   preload() {
   }
 
   update() {
-    //this.sprite.x += 0.01;
+    this.sprite.body.velocity.x = 0;
+    this.sprite.body.velocity.y = 0;
+
+    if (this.cursors.left.isDown)
+    {
+        this.sprite.body.velocity.x = -150;
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.sprite.body.velocity.x = 150;
+    }
+
+    if (this.cursors.up.isDown)
+    {
+        this.sprite.body.velocity.y = -150;
+    }
+    else if (this.cursors.down.isDown)
+    {
+        this.sprite.body.velocity.y = 150;
+    }
   }
 }
 
