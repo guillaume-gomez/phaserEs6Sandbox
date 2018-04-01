6ed75352d5eb12a9ac1bffbf750205db26fe601c
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-01 18:10:50 +0200

    randomize params in each item

--- src/rough/states/GameState.js
+++ src/rough/states/GameState.js
@@ -4,7 +4,7 @@ class GameState extends Phaser.State {
 
   create() {
     this.game.time.advancedTiming = true;
-    this.game.stage.backgroundColor = "#4488AA";
+    this.game.stage.backgroundColor = "#DDDDDD";
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
     this.game.physics.arcade.gravity.y = 300;
@@ -12,36 +12,39 @@ class GameState extends Phaser.State {
     // rough sprite generator
     const rsg = new RoughSpriteGenerator(this.game);
 
-    this.character = rsg.getRectangleSprite(100, 20, 50, 50);
-    this.game.physics.enable(this.character, Phaser.Physics.ARCADE);
-    this.game.add.existing(this.character);
-
     this.group = this.game.add.group();
-    for(let i=0; i< 20; i++) {
+    for(let i=0; i < 50; i++) {
       const rnd = Math.random();
       const x = this.getRandomInt(0, this.game.width);
-      const y = this.getRandomInt(0, 100);
+      const y = this.getRandomInt(0, 200);
+      const config = {
+        fill: this.getRandomColor(),
+        fillWeight: this.getRandomInt(1, 5)
+      }
       let sprite = null;
       if(rnd > 0.5) {
         const radius = this.getRandomInt(20, 50)
-        sprite = rsg.getCircleSprite(x, y, radius);
+        sprite = rsg.getCircleSprite(x, y, radius, config);
       } else {
         const width = this.getRandomInt(20, 50);
         const height = this.getRandomInt(20, 50);
-        sprite = rsg.getRectangleSprite(x, y, width, height);
+        sprite = rsg.getRectangleSprite(x, y, width, height, config);
       }
+      this.game.physics.enable(sprite, Phaser.Physics.ARCADE);
+      sprite.body.bounce.y = 0.80;
       this.group.add(sprite);
     }
-    //this.circle = rsg.getCircleSprite(300, 200, 30);
-    this.game.physics.enable(this.group, Phaser.Physics.ARCADE);
-    //this.game.add.existing(this.sprite2);
+    //this.game.physics.enable(this.group, Phaser.Physics.ARCADE);
+
+    this.character = rsg.getRectangleSprite(100, 20, 50, 50, {fillStyle: "solid", fill: "#D84315"});
+    this.game.physics.enable(this.character, Phaser.Physics.ARCADE);
+    this.game.add.existing(this.character);
 
     this.ground = rsg.getRectangleSprite(0, this.game.height - 32 , this.game.width, 30);
     this.game.physics.enable(this.ground, Phaser.Physics.ARCADE);
     this.ground.body.allowGravity = false;
     this.ground.body.immovable = true;
     this.game.add.existing(this.ground);
-
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
   }
@@ -70,6 +73,15 @@ class GameState extends Phaser.State {
 
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 }
 
