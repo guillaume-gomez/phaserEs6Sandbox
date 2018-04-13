2b40ca4329d08f1c867a77b0ca24437eff4147cc
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-13 18:46:08 +0200

    almost finish sun

--- src/rough/object/Sun.js
+++ src/rough/object/Sun.js
@@ -7,11 +7,23 @@ constructor(game, roughSpriteGenerator, x, y, radius) {
     const line2 = roughSpriteGenerator.getLineSprite(x + 2 * radius, y + radius + radius/2, 0, 0, radius/2, 0);
     const line3 = roughSpriteGenerator.getLineSprite(x + radius + radius/2, y + radius, 0, 0, 0, radius/2);
     const line4 = roughSpriteGenerator.getLineSprite(x + radius + radius/2, y + 2 * radius + radius/2, 0, 0, 0, radius/2);
+
+    const line5 = roughSpriteGenerator.getLineSprite(x + 2 * radius, y + radius, 0, 0, radius/2, radius/2);
+    //const line6 = roughSpriteGenerator.getLineSprite(0, y + radius, radius/2, radius/2, 0, 0);
+
+    const line7 = roughSpriteGenerator.getLineSprite(x + 2 * radius, y + 2 * radius, 0, 0, radius/2, -radius/2);
+    //const line8 = roughSpriteGenerator.getLineSprite(x + 2 * radius, y + radius, 0, 0, radius/2, radius/2);
+
+
     this.add(core);
     this.add(line);
     this.add(line2);
     this.add(line3);
     this.add(line4);
+    this.add(line5);
+    //this.add(line6);
+    this.add(line7);
+    //this.add(line8);
   }
 }
 
