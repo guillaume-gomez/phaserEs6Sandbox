f3523d2ff2af0ee9793f0c6f3e7b2f77bbc041f0
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-04-28 02:21:08 +0200

    set default config

--- src/rough/object/Building.js
+++ src/rough/object/Building.js
@@ -1,14 +1,34 @@
 class Building extends Phaser.Group {
 
-  constructor(game, roughSpriteGenerator, x, y, width, height) {
+  constructor(game, roughSpriteGenerator, x, y, width, height, config = {doorConfig: {}, windowConfig: {}, wallConfig: {}}) {
     super(game);
     // basement
-    const base = roughSpriteGenerator.getRectangleSprite(x, y, width, height);
+    const defaultWallConfig = {
+        fill: 'rgba(200,200,200,0.8)',
+        fillStyle: 'solid'
+    };
+    const base = roughSpriteGenerator.getRectangleSprite(
+        x,
+        y,
+        width,
+        height,
+        Object.assign({}, defaultWallConfig, config.wallConfig)
+    );
 
     // door
     const doorHeight = height * 0.1;
     const doorWidth = width * 0.15;
-    const door = roughSpriteGenerator.getRectangleSprite(x + width/2 - doorWidth/2, y + height - doorHeight, doorWidth, doorHeight);
+    const defaultDoorConfig = {
+        fill: 'rgba(0,0,0,0.8)',
+        fillStyle: 'solid'
+    };
+    const door = roughSpriteGenerator.getRectangleSprite(
+        x + width/2 - doorWidth/2,
+        y + height - doorHeight,
+        doorWidth,
+        doorHeight,
+        Object.assign({}, defaultDoorConfig, config.doorConfig)
+    );
     this.add(base);
     this.add(door);
 
@@ -22,9 +42,19 @@ class Building extends Phaser.Group {
     const offsetHeight = (height - doorHeight - (nbFloors * (windowHeight + spaces)) + spaces) / 2;
     const offsetWidth = (width - (nbWindowsByFloor * (windowWidth + spaces)) + spaces) / 2;
 
+    const defaultWindowConfig = {
+        fill: 'rgba(182,211,223,1)',
+        fillStyle: 'solid'
+    };
     for(let i = 0; i < nbFloors; ++i) {
       for(let j = 0; j < nbWindowsByFloor; ++j) {
-        const newWindow = roughSpriteGenerator.getRectangleSprite(x + offsetWidth + j * ( windowWidth + spaces), y + offsetHeight + i * (windowHeight + spaces), windowWidth, windowHeight);
+        const newWindow = roughSpriteGenerator.getRectangleSprite(
+            x + offsetWidth + j * ( windowWidth + spaces),
+            y + offsetHeight + i * (windowHeight + spaces),
+            windowWidth,
+            windowHeight,
+            Object.assign({}, defaultWindowConfig, config.windowConfig)
+        );
         this.add(newWindow);
       }
     }
